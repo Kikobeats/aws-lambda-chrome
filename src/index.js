a9cc880f4a05a524b098addf8dc640e56b173f62
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-02-09 20:34:52 +0000

    Fix path

--- src/index.js
+++ src/index.js
@@ -4,20 +4,17 @@ const existsFile = require('exists-file')
 const decompress = require('decompress')
 const path = require('path')
 
-const CHROME_ORIGIN_PATH = path.resolve('dist/headless_shell.zip')
+const CHROME_ORIGIN_PATH = path.join(__dirname, '../dist/headless_shell.zip')
 
 module.exports = ({
   path: tempPath = path.join(__dirname, '../dist')
 } = {}) => {
   if (!path) throw new TypeError('Need to provide a `path` for extracting.')
-  const binaryPath = path.join(tempPath, 'headless_shell', 'headless_shell')
   const extractPath = path.join(tempPath, 'headless_shell')
-  const distPath = path.join(tempPath)
 
   return async () => {
-    const isExtracted = await existsFile(extractPath)
-    if (isExtracted) return binaryPath
-    await decompress(CHROME_ORIGIN_PATH, distPath)
-    return binaryPath
+    if (await existsFile(extractPath)) return extractPath
+    await decompress(CHROME_ORIGIN_PATH, tempPath)
+    return extractPath
   }
 }
